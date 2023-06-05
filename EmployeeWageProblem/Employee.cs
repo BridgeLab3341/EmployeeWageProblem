6f20f9203355b257ed7d16eb5089fdc1f8d1d946
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-05 13:28:59 +0530

    [somu]Add:Checking Employee is Present or Absent.

---
+++ EmployeeWageProblem/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class Employee
+    {
+        public void CheckEmployeeAttendance()
+        {
+            Random random = new Random();
+            int attendance = random.Next(0, 2);
+            if (attendance == 0)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeWageProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Problem");
+            Console.WriteLine("--------------------------------");
+            Employee employee = new Employee();
+            employee.CheckEmployeeAttendance();
+        }
+    }
+}
